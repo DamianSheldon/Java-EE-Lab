60dafdab64f742ddc4aac8c701698533ecdd7e87
Meiliang Dong <dongmeilianghy@sina.com>
2018-06-15 15:59:31 +0800

    Comment code for Custom base repository instead remove.

--- order/src/main/java/com/tenneshop/order/repository/OrderRepository.java
+++ order/src/main/java/com/tenneshop/order/repository/OrderRepository.java
@@ -3,9 +3,11 @@ package com.tenneshop.order.repository;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.NoRepositoryBean;
 
 import com.tenneshop.order.entity.CustomerOrder;
 
+//@NoRepositoryBean
 public interface OrderRepository extends CrudRepository<CustomerOrder, Long> {
 	List<CustomerOrder> findAllOrdersByOrderId(String orderId);
 }
