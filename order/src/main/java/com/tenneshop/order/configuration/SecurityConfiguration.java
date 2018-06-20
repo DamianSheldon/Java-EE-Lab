3fa80b5b94e7f8db4e23029ac8bad979b3f0c215
Meiliang Dong <dongmeilianghy@sina.com>
2018-06-20 11:40:56 +0800

    USING THE H2 DATABASE CONSOLE IN SPRING BOOT WITH SPRING SECURITY.

--- order/src/main/java/com/tenneshop/order/configuration/SecurityConfiguration.java
+++ order/src/main/java/com/tenneshop/order/configuration/SecurityConfiguration.java
@@ -9,5 +9,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        
+        /*
+         * Reference:https://springframework.guru/using-the-h2-database-console-in-spring-boot-with-spring-security/
+         * To enable access to the H2 database console under Spring Security you need to change three things:
+         * 1. Allow all access to the url path /console/*.
+		 * 2. Disable CRSF (Cross-Site Request Forgery). By default, Spring Security will protect against CRSF attacks.
+		 * 3. Since the H2 database console runs inside a frame, you need to enable this in in Spring Security.
+         * */
+        httpSecurity.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
 	}
 }
--- order/src/main/java/com/tenneshop/order/repository/OrderRepositoryImpl.java
+++ order/src/main/java/com/tenneshop/order/repository/OrderRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.tenneshop.order.repository;
 import java.io.Serializable;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 
@@ -11,15 +13,52 @@ import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tenneshop.order.entity.CustomerOrder;
+import com.tenneshop.order.entity.Part;
 
 @Repository
 public class OrderRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> {
 
+	private EntityManager em;
+
+	private static final Logger logger = Logger.getLogger(OrderRepositoryImpl.class.getName());
+
 	public OrderRepositoryImpl(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
 		super(entityInformation, entityManager);
+		
+		em = entityManager;
 	}
 	
 	public void prepopulateTestOrders() {
-		
+        createPart("1234-5678-01", 1, "ABC PART",
+                new java.util.Date(), "PARTQWERTYUIOPASXDCFVGBHNJMKL", null);
+        createPart("9876-4321-02", 2, "DEF PART",
+                new java.util.Date(), "PARTQWERTYUIOPASXDCFVGBHNJMKL", null);
+        createPart("5456-6789-03", 3, "GHI PART",
+                new java.util.Date(), "PARTQWERTYUIOPASXDCFVGBHNJMKL", null);
+        createPart("ABCD-XYZW-FF", 5, "XYZ PART",
+                new java.util.Date(), "PARTQWERTYUIOPASXDCFVGBHNJMKL", null);
+        createPart("SDFG-ERTY-BN", 7, "BOM PART",
+                new java.util.Date(), "PARTQWERTYUIOPASXDCFVGBHNJMKL", null);
 	}
+	    
+    public void createPart(String partNumber,
+            int revision,
+            String description,
+            java.util.Date revisionDate,
+            String specification,
+            Serializable drawing) {
+        try {
+            Part part = new Part(partNumber,
+                    revision,
+                    description,
+                    revisionDate,
+                    specification,
+                    drawing);
+            logger.log(Level.INFO, "Created part {0}-{1}", new Object[]{partNumber, revision});
+            em.persist(part);
+            logger.log(Level.INFO, "Persisted part {0}-{1}", new Object[]{partNumber, revision});
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
