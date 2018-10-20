9ac43f7f27363f25bd13645cfc85e68b117b430c
Meiliang Dong <dongmeilianghy@sina.com>
2018-10-20 16:00:39 +0800

    Temporary enforce HTTP redirect to HTTPS via a different method from DemoSite.

--- TenneSite/site/src/main/java/com/tenneshop/configuration/SiteConfig.java
+++ TenneSite/site/src/main/java/com/tenneshop/configuration/SiteConfig.java
@@ -1,6 +1,9 @@
 package com.tenneshop.configuration;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -9,6 +12,17 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SiteConfig {
+	
+	/* FIXME:How DemoSite enforce HTTP redirect to HTTPS?
+	 * Current solution refer to 
+	 * https://stackoverflow.com/questions/26655875/spring-boot-redirect-http-to-https
+	 * 
+	 * Broadleaf official developer phillipuniverse said they configure via applicationContext-security.xml
+	 * but it seems doesn't in new version.
+	 * Details at http://forum.broadleafcommerce.org/viewtopic.php?t=3150
+	 */
+	@Value("${server.port:9443}")
+	int httpsServerPort;
 	
 	/**
      * Spring Boot does not support the configuration of both an HTTP connector and an HTTPS connector via properties.
@@ -20,14 +34,38 @@ public class SiteConfig {
      */
     @Bean
     public EmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory(@Value("${http.server.port:9090}") int httpServerPort) {
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector(httpServerPort));
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        
+        tomcat.addAdditionalTomcatConnectors(createRedirectConnector(httpServerPort, httpsServerPort));
+
+//        tomcat.addAdditionalTomcatConnectors(createStandardConnector(httpServerPort));
         return tomcat;
     }
     
-    private Connector createStandardConnector(int port) {
+//    private Connector createStandardConnector(int port) {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setPort(port);
+//        return connector;
+//    }
+    
+    private Connector createRedirectConnector(@Value("${http.server.port:9090}") int httpServerPort,
+    		int httpsServerPort) {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(port);
+        connector.setScheme("http");
+        connector.setPort(httpServerPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsServerPort);
         return connector;
     }
 }
--- TenneSite/site/src/main/java/com/tenneshop/configuration/SiteSecurityConfig.java
+++ TenneSite/site/src/main/java/com/tenneshop/configuration/SiteSecurityConfig.java
@@ -1,27 +1,32 @@
 package com.tenneshop.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SiteSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//		.requiresChannel()
-//			.anyRequest()
-//			.requiresSecure()
-//			.and()
-//		.authorizeRequests()
-//			.anyRequest()
-//			.authenticated()
-//			.and()
-//		.formLogin()
-//			.and()
-//		.httpBasic();
-//	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.formLogin()
+				.permitAll()
+				.and()
+			.httpBasic()
+				.and()
+			.authorizeRequests()
+				.anyRequest()
+				.authenticated()
+				.and()
+			.requiresChannel()
+				.anyRequest()
+				.requiresSecure();
+	}
 
 	
 }
