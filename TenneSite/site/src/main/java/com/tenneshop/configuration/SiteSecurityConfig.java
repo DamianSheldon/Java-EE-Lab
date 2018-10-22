e4c0df4f460ad8c844b88288e83e0654494f1140
Meiliang Dong <dongmeilianghy@sina.com>
2018-10-22 11:05:03 +0800

    Fix blAuthenticationManager bean not found

--- TenneSite/site/src/main/java/com/tenneshop/configuration/SiteSecurityConfig.java
+++ TenneSite/site/src/main/java/com/tenneshop/configuration/SiteSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.tenneshop.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,5 +30,9 @@ public class SiteSecurityConfig extends WebSecurityConfigurerAdapter {
 				.requiresSecure();
 	}
 
-	
+	@Bean(name="blAuthenticationManager")
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManager();
+    }
 }
