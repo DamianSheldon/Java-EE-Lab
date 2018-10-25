eac9ca92c43e1d86ec7734502a6ff9c44ab81860
Meiliang Dong <dongmeilianghy@sina.com>
2018-10-25 14:15:05 +0800

    Link js, css with homepage.

--- TenneSite/site/src/main/java/com/tenneshop/configuration/SiteSecurityConfig.java
+++ TenneSite/site/src/main/java/com/tenneshop/configuration/SiteSecurityConfig.java
@@ -3,21 +3,41 @@ package com.tenneshop.configuration;
 import javax.annotation.Resource;
 import javax.servlet.Filter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan({"org.broadleafcommerce.common.web.security","org.broadleafcommerce.profile.web.core.security",
+	"org.broadleafcommerce.core.web.order.security"})
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SiteSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Resource(name="blSessionFixationProtectionFilter")
     protected Filter sessionFixationProtectionFilter;
+	
+	@Value("${asset.server.url.prefix.internal}")
+    protected String assetServerUrlPrefixInternal;
+	
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+            .ignoring()
+                .antMatchers("/css/**")
+                .antMatchers("/fonts/**")
+                .antMatchers("/img/**")
+                .antMatchers("/js/**")
+                .antMatchers("/**/"+assetServerUrlPrefixInternal+"/**")
+                .antMatchers("/favicon.ico");
+    }
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
--- TenneSite/site/src/main/java/com/tenneshop/configuration/SiteServletConfig.java
+++ TenneSite/site/src/main/java/com/tenneshop/configuration/SiteServletConfig.java
@@ -1,6 +1,11 @@
 package com.tenneshop.configuration;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
 import org.broadleafcommerce.cms.web.PageHandlerMapping;
+import org.broadleafcommerce.common.extensibility.context.merge.Merge;
 import org.broadleafcommerce.common.web.controller.annotation.EnableAllFrameworkControllers;
 import org.broadleafcommerce.common.web.filter.FilterOrdered;
 import org.broadleafcommerce.common.web.filter.IgnorableOpenEntityManagerInViewFilter;
@@ -10,13 +15,22 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 
 @Configuration
 @EnableAllFrameworkControllers
 public class SiteServletConfig extends WebMvcConfigurerAdapter {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico")
+            .addResourceLocations("classpath:/favicon.ico");
+    }
+    
     /**
      * Setup the "blPU" entity manager on the request thread using the entity-manager-in-view pattern
      */
@@ -28,6 +42,44 @@ public class SiteServletConfig extends WebMvcConfigurerAdapter {
         registrationBean.setName("openEntityManagerInViewFilter");
         registrationBean.setOrder(FilterOrdered.PRE_SECURITY_HIGH);
         return registrationBean;
+    }
+    
+    @Bean
+    public HttpSessionEventPublisher sessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
+    
+    @Bean
+    public HandlerMapping staticResourcesHandlerMapping() {
+        SimpleUrlHandlerMapping resourceMapping = new SimpleUrlHandlerMapping();
+        resourceMapping.setOrder(-10);
+        Properties mappings = new Properties();
+        mappings.put("/js/**", "blJsResources");
+        mappings.put("/css/**", "blCssResources");
+        mappings.put("/img/**", "blImageResources");
+        mappings.put("/fonts/**", "blFontResources");
+        resourceMapping.setMappings(mappings);
+        return resourceMapping;
+    }
+    
+    @Merge("blJsLocations")
+    public List<String> jsLocations() {
+        return Collections.singletonList("classpath:/js/");
+    }
+
+    @Merge("blCssLocations")
+    public List<String> cssLocations() {
+        return Collections.singletonList("classpath:/css/");
+    }
+
+    @Merge("blImageLocations")
+    public List<String> imageLocations() {
+        return Collections.singletonList("classpath:/img/");
+    }
+
+    @Merge("blFontLocations")
+    public List<String> fontLocations() {
+        return Collections.singletonList("classpath:/fonts/");
     }
     
     @Bean
