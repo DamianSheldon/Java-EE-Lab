e44a728d99b6ba4dd8bd80f01a10da1a57a1bf0b
Meiliang Dong <dongmeilianghy@sina.com>
2018-10-26 09:41:50 +0800

    Set up message sources.

--- TenneSite/site/src/main/java/com/tenneshop/configuration/SiteConfig.java
+++ TenneSite/site/src/main/java/com/tenneshop/configuration/SiteConfig.java
@@ -1,18 +1,80 @@
 package com.tenneshop.configuration;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.broadleafcommerce.common.extensibility.context.merge.Merge;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+//import com.community.core.config.StringFactoryBean;
+//import com.mycompany.checkout.service.workflow.RecordHeatRangeActivity;
 
 @Configuration
 public class SiteConfig {
 	
+//    @Bean
+//    @ConditionalOnProperty("jmx.app.name")
+//    public StringFactoryBean blJmxNamingBean() {
+//        return new StringFactoryBean();
+//    }
+
+    @Merge("blMessageSourceBaseNames")
+    public List<String> customMessages() {
+        return Arrays.asList("classpath:messages");
+    }
+    
+//    /**
+//     * Disables caching only in the 'default' profile which is useful for development.
+//     */
+//    @Merge("blMergedCacheConfigLocations")
+//    @Profile("default")
+//    public List<String> removeCachingConfiguration() {
+//        return Arrays.asList("classpath:bl-override-ehcache.xml");
+//    }
+    
+    /**
+     * Broadleaf Commerce comes with an Image Server that allows you to manipulate images.   For example, the 
+     *  demo includes a high resolution image for each product that is reduced in size for browsing operations
+     */
+    @Merge("blStaticMapNamedOperations")
+    public Map<String, Map<String, String>> customImageOperations() {
+        Map<String, Map<String, String>> operations = new HashMap<>();
+        Map<String, String> browseOperation = new HashMap<>();
+        browseOperation.put("resize-width-amount", "400");
+        browseOperation.put("resize-height-amount", "400");
+        browseOperation.put("resize-high-quality", "false");
+        browseOperation.put("resize-maintain-aspect-ratio", "true");
+        browseOperation.put("resize-reduce-only", "true");
+        operations.put("browse", browseOperation);
+        
+        Map<String, String> thumbnailOperation = new HashMap<>();
+        thumbnailOperation.put("resize-width-amount", "60");
+        thumbnailOperation.put("resize-height-amount", "60");
+        thumbnailOperation.put("resize-high-quality", "false");
+        thumbnailOperation.put("resize-maintain-aspect-ratio", "true");
+        thumbnailOperation.put("resize-reduce-only", "true");
+        operations.put("thumbnail", thumbnailOperation);
+        
+        return operations;
+    }
+    
+//    @Merge("blCheckoutWorkflowActivities")
+//    public List<?> customCheckoutActivities(RecordHeatRangeActivity recordHeatRangeActivity) {
+//        return Arrays.asList(recordHeatRangeActivity);
+//    }
+    
 	/* FIXME:How DemoSite enforce HTTP redirect to HTTPS?
 	 * Current solution refer to 
 	 * https://stackoverflow.com/questions/26655875/spring-boot-redirect-http-to-https
