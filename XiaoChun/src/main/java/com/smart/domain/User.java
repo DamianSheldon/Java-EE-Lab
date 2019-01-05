58b6e06fef4c40c608b392293ba4f9df5c619ab6
Meiliang Dong <dongmeilianghy@sina.com>
2019-01-05 16:52:05 +0800

    feat:Improve User entity

--- XiaoChun/src/main/java/com/smart/domain/User.java
+++ XiaoChun/src/main/java/com/smart/domain/User.java
@@ -1,5 +1,11 @@
 package com.smart.domain;
 
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Entity;
+
+@Entity
 public class User extends BaseDomain {
 
 	/**
@@ -18,4 +24,82 @@ public class User extends BaseDomain {
 	private long locked;
 	
 	private int credit;
+	
+	private Date lastVisit;
+	
+	private String lastTip;
+	
+	private Set manBoards;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public long getUserType() {
+		return userType;
+	}
+
+	public void setUserType(long userType) {
+		this.userType = userType;
+	}
+
+	public long getLocked() {
+		return locked;
+	}
+
+	public void setLocked(long locked) {
+		this.locked = locked;
+	}
+
+	public int getCredit() {
+		return credit;
+	}
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public Date getLastVisit() {
+		return lastVisit;
+	}
+
+	public void setLastVisit(Date lastVisit) {
+		this.lastVisit = lastVisit;
+	}
+
+	public String getLastTip() {
+		return lastTip;
+	}
+
+	public void setLastTip(String lastTip) {
+		this.lastTip = lastTip;
+	}
+
+	public Set getManBoards() {
+		return manBoards;
+	}
+
+	public void setManBoards(Set manBoards) {
+		this.manBoards = manBoards;
+	}
 }
