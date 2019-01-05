286402ae9cf0eadc16fd44ab81cb92bead19f685
Meiliang Dong <dongmeilianghy@sina.com>
2019-01-05 16:40:26 +0800

    feat:Improve Post entity

--- XiaoChun/src/main/java/com/smart/domain/Post.java
+++ XiaoChun/src/main/java/com/smart/domain/Post.java
@@ -1,9 +1,12 @@
 package com.smart.domain;
 
-import java.sql.Date;
+import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.Cache;
@@ -34,7 +37,68 @@ public class Post extends BaseDomain {
 	@Column(name = "create_date")
 	private Date createDate;
 	
-	// TODO:  User
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
 	
+	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+	@JoinColumn(name = "topic_id")
+	private Topic topic;
+
+	public int getPostId() {
+		return postId;
+	}
+
+	public void setPostId(int postId) {
+		this.postId = postId;
+	}
+
+	public String getPostTitle() {
+		return postTitle;
+	}
+
+	public void setPostTitle(String postTitle) {
+		this.postTitle = postTitle;
+	}
+
+	public String getPostText() {
+		return postText;
+	}
+
+	public void setPostText(String postText) {
+		this.postText = postText;
+	}
+
+	public int getBoardId() {
+		return boardId;
+	}
+
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Topic getTopic() {
+		return topic;
+	}
+
+	public void setTopic(Topic topic) {
+		this.topic = topic;
+	}
 	
 }
