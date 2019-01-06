c595274aaed94010362a28a36293ee35f25ec80d
Meiliang Dong <dongmeilianghy@sina.com>
2019-01-06 17:50:45 +0800

    feat: add TopicDao

---
+++ XiaoChun/src/main/java/com/smart/dao/TopicDao.java
@@ -0,0 +1,24 @@
+package com.smart.dao;
+
+import com.smart.domain.Topic;
+
+public class TopicDao extends BaseDao<Topic> {
+
+	private static final String GET_BOARD_DIGEST_TOPIC = "from Topic t where t.boardId = ? and digest > 0 "
+			+ "order by t.lastPost desc, digest desc";
+	private static final String GET_PAGED_TOPIC = "from Topic where boardId = ? order by lastPost desc";
+	private static final String QUERY_TOPIC_BY_TITLE = "from Topic where topicTitle like ? order by lastPost desc";
+	
+	public Page getBoardDigestTopics(int boardId, int pageNo, int pageSize) {
+		return pagedQuery(GET_BOARD_DIGEST_TOPIC, pageNo, pageSize, boardId);
+	}
+	
+	public Page getPagedTopics(int boardId, int pageNo, int pageSize) {
+		return pagedQuery(GET_PAGED_TOPIC, pageNo, pageSize, boardId);
+	}
+	
+	public Page queryTopicByTitle(String title, int pageNo, int pageSize) {
+		return pagedQuery(QUERY_TOPIC_BY_TITLE, pageNo, pageSize, title);
+	}
+	
+}
