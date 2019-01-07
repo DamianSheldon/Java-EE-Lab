79b9af355a4870014250c6d4734f94fe0c81028d
Meiliang Dong <dongmeilianghy@sina.com>
2019-01-07 17:17:22 +0800

    feat:add nessary import

--- XiaoChun/src/main/java/com/smart/domain/Board.java
+++ XiaoChun/src/main/java/com/smart/domain/Board.java
@@ -1,5 +1,8 @@
 package com.smart.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
