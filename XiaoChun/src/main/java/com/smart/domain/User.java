fd42923f7d2d3ccef2e80ee90f3858ff9d7ead4b
Meiliang Dong <dongmeilianghy@sina.com>
2019-01-08 09:18:35 +0800

    feat:improve User entity

--- XiaoChun/src/main/java/com/smart/domain/User.java
+++ XiaoChun/src/main/java/com/smart/domain/User.java
@@ -1,9 +1,19 @@
 package com.smart.domain;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,23 +25,49 @@ public class User extends BaseDomain {
 	 */
 	private static final long serialVersionUID = -7471521913490203758L;
 
+    /**
+     *锁定用户对应的状态值 
+     */
+    public static final int USER_LOCK = 1;
+    /**
+     * 用户解锁对应的状态值
+     */
+    public static final int USER_UNLOCK = 0;
+    /**
+     * 管理员类型
+     */
+    public static final int FORUM_ADMIN = 2;
+    /**
+     * 普通用户类型
+     */
+    public static final int NORMAL_USER = 1;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
 	private int userId;
-	
+
+    @Column(name = "user_name")
 	private String userName;
 	
 	private String password;
 	
-	private long userType;
+    @Column(name = "user_type")
+	private long userType = NORMAL_USER;
 	
 	private long locked;
 	
 	private int credit;
 	
+    @Column(name = "last_visit")
 	private Date lastVisit;
-	
-	private String lastTip;
-	
-	private Set manBoards;
+
+    @Column(name = "last_ip")
+	private String lastIp;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "t_board_manager", joinColumns = {@JoinColumn(name = "user_id")}, inverseJoinColumns = {@JoinColumn(name = "board_id")})
+	private Set<Board> manBoards = new HashSet<Board>();
 
 	public int getUserId() {
 		return userId;
@@ -89,19 +125,19 @@ public class User extends BaseDomain {
 		this.lastVisit = lastVisit;
 	}
 
-	public String getLastTip() {
-		return lastTip;
+	public String getLastIp() {
+		return lastIp;
 	}
 
-	public void setLastTip(String lastTip) {
-		this.lastTip = lastTip;
+	public void setLastIp(String lastIp) {
+		this.lastIp = lastIp;
 	}
 
-	public Set getManBoards() {
+	public Set<Board> getManBoards() {
 		return manBoards;
 	}
 
-	public void setManBoards(Set manBoards) {
+	public void setManBoards(Set<Board> manBoards) {
 		this.manBoards = manBoards;
 	}
 }
