66b163c0c38269e4cc1b497a1f05b9dec9bf586c
Meiliang Dong <dongmeilianghy@sina.com>
2019-01-10 16:06:53 +0800

    test:add register service test

---
+++ XiaoChun/src/test/java/com/smart/service/UserServiceTests.java
@@ -0,0 +1,67 @@
+package com.smart.service;
+
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.testng.Assert.assertEquals;
+
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.smart.dao.UserDao;
+import com.smart.domain.User;
+import com.smart.exception.UserExistException;
+
+public class UserServiceTests extends BaseServiceTest {
+    private UserDao userDao;
+    private UserService userService;
+
+    @BeforeClass
+    public void init() {
+        userDao = mock(UserDao.class);
+        userService = new UserService();
+        ReflectionTestUtils.setField(userService, "userDao", userDao);
+    }
+
+    @Test
+    public void testRegister() throws UserExistException {
+        User user = new User();
+        user.setUserName("testwww");
+        user.setPassword("1234");
+
+        doAnswer(new Answer<User>() {
+            public User answer(InvocationOnMock invocation) {
+                Object[] args = invocation.getArguments();
+                User user = (User) args[0];
+                if (user != null) {
+                    user.setUserId(1);
+                }
+                return user;
+            }
+        }).when(userDao).save(user);
+
+        userService.register(user);
+        assertEquals(user.getUserId(), 1);
+
+        verify(userDao, times(1)).save(user);
+    }
+
+    @Test
+    public void testGetUserByUserName() {
+        
+    }
+
+    @Test
+    public void lockUser() {
+    
+    }
+
+    @Test
+    public void unlockUser() {
+    
+    }
+}
