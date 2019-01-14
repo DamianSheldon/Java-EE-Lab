abc0afcd178f99cccf57106b8ec2571c9f992743
Meiliang Dong <dongmeilianghy@sina.com>
2019-01-14 15:09:34 +0800

    test:list all boards

---
+++ XiaoChun/src/test/java/com/smart/web/controller/ForumManagerControllerTests.java
@@ -0,0 +1,62 @@
+package com.smart.web.controller;
+
+import static org.junit.Assert.assertNotNull;
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.testng.annotations.Test;
+import org.unitils.spring.annotation.SpringBeanByType;
+
+import com.smart.domain.Board;
+import com.smart.web.ForumManagerController;
+
+public class ForumManagerControllerTests extends BaseWebTest {
+    @SpringBeanByType
+    private ForumManagerController controller;
+
+    @Test
+    public void listAllBoards() throws Exception {
+        request.setRequestURI("/index");
+        request.setMethod("GET");
+
+        ModelAndView mav = controller.listAllBoards();
+        List<Board> boards = (List<Board>) mav.getModelMap().get("boards");
+
+        assertNotNull(mav);
+        assertEquals(mav.getViewName(), "/listAllBoards");
+        assertNotNull(boards);
+    }
+
+    @Test
+    public void addBoardPage() throws Exception {
+    
+    }
+
+    @Test
+    public void addBoard() throws Exception {
+    
+    }
+
+    @Test
+    public void setBoardManagerPage() throws Exception {
+    
+    }
+
+    @Test
+    public void setBoardManager() throws Exception {
+    
+    }
+
+    @Test
+    public void userLockManagePage() throws Exception {
+    
+    }
+
+    @Test
+    public void userLockManage() throws Exception {
+    
+    }
+
+}
